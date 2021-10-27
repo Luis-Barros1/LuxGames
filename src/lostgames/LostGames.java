c6ccdc5e4a8049eea5df1b13dd7ed810f7de499a
Luis Barros <luisrochabarros@hotmail.com>
2021-10-27 19:11:05 -0300

    First Commit

---
+++ src/lostgames/LostGames.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lostgames;
+
+/**
+ *
+ * @author Luis
+ */
+public class LostGames {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        tela_Login_new tela_Login_new = new tela_Login_new();
+        tela_Login_new.setVisible(true);
+    }
+    
+}
